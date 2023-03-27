52a883e46d2f8a927b0c7cc81f12e1c7eea67ca7
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-03-27 16:26:13 +0200

    Fix DDL3 for view creation & added eservice_probing_response entity class

--- src/main/resources/db/migration/V3__DDL.sql
+++ src/main/resources/db/migration/V3__DDL.sql
@@ -2,5 +2,5 @@ CREATE VIEW eservice_view AS
 SELECT e.id, e.eservice_id , e.eservice_name, e.producer_name , e.version_id , e.state ,e.probing_enabled , e.version_number , epr.response_received
 FROM eservices e
 LEFT JOIN eservice_probing_responses epr ON epr.eservice_id = e.id
-GROUP BY e.eservice_name, e.producer_name , e.version_id , e.state, e.id;
+GROUP BY e.eservice_name, e.producer_name , e.version_id , e.state, e.id, epr.response_received;
 
