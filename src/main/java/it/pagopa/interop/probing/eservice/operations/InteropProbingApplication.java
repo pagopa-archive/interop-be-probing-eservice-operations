b8d13770773ff5dc324b626211d387675407d7eb
acolucci <a.colucci@dxc.com>
2023-05-04 15:12:08 +0200

    set the default timezone as UTC

--- src/main/java/it/pagopa/interop/probing/eservice/operations/InteropProbingApplication.java
+++ src/main/java/it/pagopa/interop/probing/eservice/operations/InteropProbingApplication.java
@@ -1,13 +1,15 @@
 package it.pagopa.interop.probing.eservice.operations;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InteropProbingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InteropProbingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    SpringApplication.run(InteropProbingApplication.class, args);
+  }
 
 }
