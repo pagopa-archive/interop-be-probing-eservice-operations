5c43bc13a37002af49d71b1403f7feaeae5567cb
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-06-14 11:16:38 +0200

    rollback previous push

--- src/main/java/it/pagopa/interop/probing/eservice/operations/tracing/config/TracingConfig.java
+++ src/main/java/it/pagopa/interop/probing/eservice/operations/tracing/config/TracingConfig.java
@@ -1,10 +1,7 @@
 package it.pagopa.interop.probing.eservice.operations.tracing.config;
 
 import javax.servlet.Filter;
-import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
@@ -15,12 +12,6 @@ public class TracingConfig {
 
   @Value("${spring.application.name}")
   private String awsXraySegmentName;
-  @Value("${spring.datasource.username}")
-  private String username;
-  @Value("${spring.datasource.password}")
-  private String password;
-  @Value("${spring.datasource.url}")
-  private String datasourceUrl;
 
   @Bean
   public Filter tracingFilter() {
@@ -33,10 +24,5 @@ public class TracingConfig {
     return HttpClientBuilder.create();
   }
 
-  @Bean
-  @ConfigurationProperties(prefix = "spring.datasource")
-  public DataSource dataSource() {
-    return DataSourceBuilder.create().driverClassName("org.postgresql.Driver").url(datasourceUrl)
-        .username(username).password(password).build();
-  }
 }
+
