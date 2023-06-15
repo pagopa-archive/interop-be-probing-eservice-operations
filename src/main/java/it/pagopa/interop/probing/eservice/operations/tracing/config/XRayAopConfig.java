87c2be36d691128817de5071410b93e3c3833bb7
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-06-15 15:11:30 +0200

    Development progress for Aurora tracing operations with xray

--- src/main/java/it/pagopa/interop/probing/eservice/operations/tracing/config/XRayAopConfig.java
+++ src/main/java/it/pagopa/interop/probing/eservice/operations/tracing/config/XRayAopConfig.java
@@ -1,6 +1,7 @@
 package it.pagopa.interop.probing.eservice.operations.tracing.config;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.MDC;
@@ -10,12 +11,22 @@ import it.pagopa.interop.probing.eservice.operations.util.logging.LoggingPlaceho
 
 @Aspect
 @Component
-public class XRayMDCConfig {
+public class XRayAopConfig {
 
   @Before("execution(* it.pagopa.interop.probing.eservice.operations.rest..*(..))")
   public void beforeController(JoinPoint joinPoint) {
     MDC.put(LoggingPlaceholders.TRACE_ID_XRAY_PLACEHOLDER,
         LoggingPlaceholders.TRACE_ID_XRAY_MDC_PREFIX
             + AWSXRay.getCurrentSegment().getTraceId().toString() + "]");
+  }
+
+  @Before("execution(* it.pagopa.interop.probing.eservice.operations.repository.query.builder..*(..))")
+  public void beforeQueryOperation(JoinPoint joinPoint) {
+    AWSXRay.beginSubsegment(LoggingPlaceholders.AURORA_SUBSEGMENT_NAME);
+  }
+
+  @After("execution(* it.pagopa.interop.probing.eservice.operations.repository.query.builder..*(..))")
+  public void afterQueryOperation(JoinPoint joinPoint) {
+    AWSXRay.endSubsegment();
   }
 }
--- src/main/java/it/pagopa/interop/probing/eservice/operations/util/logging/LoggingPlaceholders.java
+++ src/main/java/it/pagopa/interop/probing/eservice/operations/util/logging/LoggingPlaceholders.java
@@ -6,4 +6,5 @@ public class LoggingPlaceholders {
   public static final String TRACE_ID_PLACEHOLDER = "trace_id";
   public static final String TRACE_ID_XRAY_PLACEHOLDER = "AWS-XRAY-TRACE-ID";
   public static final String TRACE_ID_XRAY_MDC_PREFIX = "- [TRACE_ID= ";
+  public static final String AURORA_SUBSEGMENT_NAME = "Aurora Operation";
 }
