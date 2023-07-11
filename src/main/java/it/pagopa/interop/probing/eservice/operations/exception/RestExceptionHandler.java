22d38690e212c43ccefb4175747585732295b3a2
acolucci <a.colucci@dxc.com>
2023-07-11 11:26:59 +0200

    Removed cid with random value in logs

--- src/main/java/it/pagopa/interop/probing/eservice/operations/exception/RestExceptionHandler.java
+++ src/main/java/it/pagopa/interop/probing/eservice/operations/exception/RestExceptionHandler.java
@@ -1,7 +1,6 @@
 package it.pagopa.interop.probing.eservice.operations.exception;
 
 import java.util.List;
-import it.pagopa.interop.probing.eservice.operations.util.logging.Logger;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -15,65 +14,60 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import it.pagopa.interop.probing.eservice.operations.dtos.Problem;
 import it.pagopa.interop.probing.eservice.operations.dtos.ProblemError;
 import it.pagopa.interop.probing.eservice.operations.util.constant.ErrorMessages;
+import it.pagopa.interop.probing.eservice.operations.util.logging.Logger;
 import it.pagopa.interop.probing.eservice.operations.util.logging.LoggingPlaceholders;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@Autowired
-	private Logger log;
-	/**
-	 * Manages the {@link EserviceNotFoundException} creating a new
-	 * {@link ResponseEntity} and sending it to the client with error code 404 and
-	 * information about the error
-	 *
-	 * @param ex The intercepted exception
-	 * @return A new {@link ResponseEntity} with {@link Problem} body
-	 */
-	@ExceptionHandler(EserviceNotFoundException.class)
-	protected ResponseEntity<Problem> handleEserviceNotFoundException(EserviceNotFoundException ex) {
-		log.logMessageException(ex);
-		Problem problemResponse = createProblem(HttpStatus.NOT_FOUND, ErrorMessages.ELEMENT_NOT_FOUND,
-				ErrorMessages.ELEMENT_NOT_FOUND);
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemResponse);
-	}
+  @Autowired
+  private Logger log;
 
-	/**
-	 * Manages the {@link HttpMessageNotReadableException} creating a new
-	 * {@link ResponseEntity} and sending it to the client with error code 400 and
-	 * information about the error
-	 *
-	 * @param ex The intercepted exception
-	 * @return A new {@link ResponseEntity} with {@link Problem} body
-	 */
-	@Override
-	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		log.logMessageException(ex);
-		Problem problemResponse = createProblem(HttpStatus.BAD_REQUEST, ErrorMessages.BAD_REQUEST,
-				ErrorMessages.BAD_REQUEST);
-		return ResponseEntity.status(status).body(problemResponse);
-	}
+  /**
+   * Manages the {@link EserviceNotFoundException} creating a new {@link ResponseEntity} and sending
+   * it to the client with error code 404 and information about the error
+   *
+   * @param ex The intercepted exception
+   * @return A new {@link ResponseEntity} with {@link Problem} body
+   */
+  @ExceptionHandler(EserviceNotFoundException.class)
+  protected ResponseEntity<Problem> handleEserviceNotFoundException(EserviceNotFoundException ex) {
+    log.logMessageException(ex);
+    Problem problemResponse = createProblem(HttpStatus.NOT_FOUND, ErrorMessages.ELEMENT_NOT_FOUND,
+        ErrorMessages.ELEMENT_NOT_FOUND);
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemResponse);
+  }
 
-	/**
-	 * Creates an instance of type {@link Problem} following the RFC 7807 standard
-	 *
-	 * @param responseCode  The response error code
-	 * @param titleMessage  The response title message
-	 * @param detailMessage The response detail error message
-	 * @return A new instance of {@link Problem}
-	 */
-	private Problem createProblem(HttpStatus responseCode, String titleMessage, String detailMessage) {
-		ProblemError errorDetails = ProblemError.builder()
-				.code(responseCode.toString())
-				.detail(detailMessage)
-				.build();
-		return Problem.builder()
-				.status(responseCode.value())
-				.title(titleMessage)
-				.detail(detailMessage)
-				.traceId(MDC.get(LoggingPlaceholders.TRACE_ID_PLACEHOLDER))
-				.errors(List.of(errorDetails))
-				.build();
-	}
+  /**
+   * Manages the {@link HttpMessageNotReadableException} creating a new {@link ResponseEntity} and
+   * sending it to the client with error code 400 and information about the error
+   *
+   * @param ex The intercepted exception
+   * @return A new {@link ResponseEntity} with {@link Problem} body
+   */
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+      HttpHeaders headers, HttpStatus status, WebRequest request) {
+    log.logMessageException(ex);
+    Problem problemResponse =
+        createProblem(HttpStatus.BAD_REQUEST, ErrorMessages.BAD_REQUEST, ErrorMessages.BAD_REQUEST);
+    return ResponseEntity.status(status).body(problemResponse);
+  }
+
+  /**
+   * Creates an instance of type {@link Problem} following the RFC 7807 standard
+   *
+   * @param responseCode The response error code
+   * @param titleMessage The response title message
+   * @param detailMessage The response detail error message
+   * @return A new instance of {@link Problem}
+   */
+  private Problem createProblem(HttpStatus responseCode, String titleMessage,
+      String detailMessage) {
+    ProblemError errorDetails =
+        ProblemError.builder().code(responseCode.toString()).detail(detailMessage).build();
+    return Problem.builder().status(responseCode.value()).title(titleMessage).detail(detailMessage)
+        .traceId(MDC.get(LoggingPlaceholders.TRACE_ID_XRAY_PLACEHOLDER))
+        .errors(List.of(errorDetails)).build();
+  }
 }
