bd9021d2817301b84eab93a1aacee265bc7ab055
AlessandroColucci <107028283+AlessandroColucci@users.noreply.github.com>
2023-07-11 13:35:43 +0200

    Removed cid with random value in logs (#32)

--- src/main/java/it/pagopa/interop/probing/eservice/operations/util/logging/LoggingPlaceholders.java
+++ src/main/java/it/pagopa/interop/probing/eservice/operations/util/logging/LoggingPlaceholders.java
@@ -3,7 +3,6 @@ package it.pagopa.interop.probing.eservice.operations.util.logging;
 public class LoggingPlaceholders {
   private LoggingPlaceholders() {}
 
-  public static final String TRACE_ID_PLACEHOLDER = "trace_id";
   public static final String TRACE_ID_XRAY_PLACEHOLDER = "AWS-XRAY-TRACE-ID";
   public static final String TRACE_ID_XRAY_MDC_PREFIX = "- [TRACE_ID= ";
   public static final String AURORA_SUBSEGMENT_NAME = "Aurora Operation";
